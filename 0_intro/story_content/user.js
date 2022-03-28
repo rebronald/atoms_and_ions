f205a9b5a9fddb0ee2f8aaa65e77ca3abe702410
RR <53237734+rebronald@users.noreply.github.com>
2022-03-29 11:56:02 +1300

    Add files via upload

---
+++ 0_intro/story_content/user.js
@@ -0,0 +1,112 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "5vlueJ85twR":
+        Script1();
+        break;
+      case "5u3Z1DW1IVh":
+        Script2();
+        break;
+      case "5s4lsFmsPA9":
+        Script3();
+        break;
+      case "5eb0Kc8yKzj":
+        Script4();
+        break;
+      case "60HNh4l0vqk":
+        Script5();
+        break;
+  }
+}
+
+function Script1()
+{
+  var player = GetPlayer();
+var slides = player.GetVar("slides");
+var total = player.GetVar("total");
+var myScore = (slides/total)*100;
+
+function findLMSAPI(win) {
+ if (win.hasOwnProperty("GetStudentID")) return win;
+ else if (win.parent == win) return null;
+ else return findLMSAPI(win.parent);
+}
+
+var lmsAPI = findLMSAPI(this);
+lmsAPI.SetScore(myScore, 100, 0);
+
+}
+
+function Script2()
+{
+  var player = GetPlayer();
+var slides = player.GetVar("slides");
+var total = player.GetVar("total");
+var myScore = (slides/total)*100;
+
+function findLMSAPI(win) {
+ if (win.hasOwnProperty("GetStudentID")) return win;
+ else if (win.parent == win) return null;
+ else return findLMSAPI(win.parent);
+}
+
+var lmsAPI = findLMSAPI(this);
+lmsAPI.SetScore(myScore, 100, 0);
+
+}
+
+function Script3()
+{
+  var player = GetPlayer();
+var slides = player.GetVar("slides");
+var total = player.GetVar("total");
+var myScore = (slides/total)*100;
+
+function findLMSAPI(win) {
+ if (win.hasOwnProperty("GetStudentID")) return win;
+ else if (win.parent == win) return null;
+ else return findLMSAPI(win.parent);
+}
+
+var lmsAPI = findLMSAPI(this);
+lmsAPI.SetScore(myScore, 100, 0);
+
+}
+
+function Script4()
+{
+  var player = GetPlayer();
+var slides = player.GetVar("slides");
+var total = player.GetVar("total");
+var myScore = (slides/total)*100;
+
+function findLMSAPI(win) {
+ if (win.hasOwnProperty("GetStudentID")) return win;
+ else if (win.parent == win) return null;
+ else return findLMSAPI(win.parent);
+}
+
+var lmsAPI = findLMSAPI(this);
+lmsAPI.SetScore(myScore, 100, 0);
+
+}
+
+function Script5()
+{
+  var player = GetPlayer();
+var slides = player.GetVar("slides");
+var total = player.GetVar("total");
+var myScore = (slides/total)*100;
+
+function findLMSAPI(win) {
+ if (win.hasOwnProperty("GetStudentID")) return win;
+ else if (win.parent == win) return null;
+ else return findLMSAPI(win.parent);
+}
+
+var lmsAPI = findLMSAPI(this);
+lmsAPI.SetScore(myScore, 100, 0);
+
+}
+
