21c39d86861dfc748c31ad5337de6d2afe44cb7d
RR <53237734+rebronald@users.noreply.github.com>
2022-03-29 15:06:50 +1300

    new upload

---
+++ 3_ionic_formula/10_writing_ionic_formula/story_content/user.js
@@ -0,0 +1,1063 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "6T4mrbC1atY":
+        Script1();
+        break;
+      case "5WTrwd8BHgw":
+        Script2();
+        break;
+      case "6nB6Rm2h2et":
+        Script3();
+        break;
+      case "5ZEtWhpYn2m":
+        Script4();
+        break;
+      case "6HyzAC2biDw":
+        Script5();
+        break;
+      case "65nQXaTaP47":
+        Script6();
+        break;
+      case "6lYq0tQNEUX":
+        Script7();
+        break;
+      case "5h1nHmPWB2B":
+        Script8();
+        break;
+      case "6UCVThxi7vS":
+        Script9();
+        break;
+      case "6NxrmeqIUwJ":
+        Script10();
+        break;
+      case "5yZCozrt5BO":
+        Script11();
+        break;
+      case "5xro1LPEeRF":
+        Script12();
+        break;
+      case "6WjYPkrT7qq":
+        Script13();
+        break;
+      case "6aOwJmQtn0v":
+        Script14();
+        break;
+      case "6X2w7yNmYbn":
+        Script15();
+        break;
+      case "5tB2XSJ4Qm5":
+        Script16();
+        break;
+      case "6Xg3bdIAPU1":
+        Script17();
+        break;
+      case "5fgqVZd0Ktb":
+        Script18();
+        break;
+      case "5yxFUqDPkKz":
+        Script19();
+        break;
+      case "6J3WkXbwdGb":
+        Script20();
+        break;
+      case "6lYxCwpV3pM":
+        Script21();
+        break;
+      case "6LiKUYDsEeZ":
+        Script22();
+        break;
+      case "6R6DKKpk2K2":
+        Script23();
+        break;
+      case "5V4lBBvqAiV":
+        Script24();
+        break;
+      case "5w3JC3zjOpb":
+        Script25();
+        break;
+      case "6knzKTiJCW4":
+        Script26();
+        break;
+      case "6bEpjwUTBp2":
+        Script27();
+        break;
+      case "5sI9SJ9zLF0":
+        Script28();
+        break;
+      case "5s5yHvnElkW":
+        Script29();
+        break;
+      case "6FlQ4SNQW2n":
+        Script30();
+        break;
+      case "5dcCY4SKGGb":
+        Script31();
+        break;
+      case "5wlGh0X3Da8":
+        Script32();
+        break;
+      case "68lBgL6R2vA":
+        Script33();
+        break;
+      case "6JewMtg1jWn":
+        Script34();
+        break;
+      case "5hzD4kaRi7y":
+        Script35();
+        break;
+      case "6JJJB6raQcz":
+        Script36();
+        break;
+      case "66YaOFKPbP8":
+        Script37();
+        break;
+      case "6UWnJHEutR5":
+        Script38();
+        break;
+      case "68jpSygYPvA":
+        Script39();
+        break;
+      case "5x6BbgCmTV4":
+        Script40();
+        break;
+      case "6mPpLZM6MPn":
+        Script41();
+        break;
+      case "6k8dONJVT89":
+        Script42();
+        break;
+      case "5rBdGLh3uF7":
+        Script43();
+        break;
+      case "5saeRWYL2g0":
+        Script44();
+        break;
+      case "6RUcsr74JnF":
+        Script45();
+        break;
+      case "5g01fAp5AxN":
+        Script46();
+        break;
+      case "62yEglLGS3k":
+        Script47();
+        break;
+      case "5VcmfwchX0R":
+        Script48();
+        break;
+      case "6QsQtQCkBSi":
+        Script49();
+        break;
+      case "6lLoi6Nv5G5":
+        Script50();
+        break;
+      case "6dt1by8Eadf":
+        Script51();
+        break;
+      case "63iFVDwtIsR":
+        Script52();
+        break;
+      case "5bzLqebiFvK":
+        Script53();
+        break;
+      case "5YXeveVyRGO":
+        Script54();
+        break;
+      case "5V2eEltDwy4":
+        Script55();
+        break;
+  }
+}
+
+function Script1()
+{
+  var els = document.getElementsByTagName('input');
+for (var i=0; i < els.length; i++) {
+ els[i].setAttribute("autocomplete", "off");
+}
+}
+
+function Script2()
+{
+  var player = GetPlayer();
+
+var a= player.GetVar("SB10_TextEntry4_sb31");
+
+var c= a+"₂";
+
+player.SetVar("SB10_TextEntry4_sb31",c);
+
+var d= c+" ";
+
+player.SetVar("SB10_TextEntry4_sb31",d);
+
+var e = d.trim();
+player.SetVar("SB10_TextEntry4_sb31",e);
+}
+
+function Script3()
+{
+  var player = GetPlayer();
+
+var a= player.GetVar("SB10_TextEntry4_sb31");
+
+var c= a+"₄";
+
+player.SetVar("SB10_TextEntry4_sb31",c);
+
+var d= c+" ";
+
+player.SetVar("SB10_TextEntry4_sb31",d);
+
+var e = d.trim();
+player.SetVar("SB10_TextEntry4_sb31",e);
+}
+
+function Script4()
+{
+  var player = GetPlayer();
+
+var a= player.GetVar("SB10_TextEntry4_sb31");
+
+var c= a+"₃";
+
+player.SetVar("SB10_TextEntry4_sb31",c);
+
+var d= c+" ";
+
+player.SetVar("SB10_TextEntry4_sb31",d);
+
+var e = d.trim();
+player.SetVar("SB10_TextEntry4_sb31",e);
+}
+
+function Script5()
+{
+  var els = document.getElementsByTagName('input');
+for (var i=0; i < els.length; i++) {
+ els[i].setAttribute("autocomplete", "off");
+}
+}
+
+function Script6()
+{
+  var player = GetPlayer();
+
+var a= player.GetVar("SB10_TextEntry5_sb32");
+
+var c= a+"₂";
+
+player.SetVar("SB10_TextEntry5_sb32",c);
+
+var d= c+" ";
+
+player.SetVar("SB10_TextEntry5_sb32",d);
+
+var e = d.trim();
+player.SetVar("SB10_TextEntry5_sb32",e);
+}
+
+function Script7()
+{
+  var player = GetPlayer();
+
+var a= player.GetVar("SB10_TextEntry5_sb32");
+
+var c= a+"₄";
+
+player.SetVar("SB10_TextEntry5_sb32",c);
+
+var d= c+" ";
+
+player.SetVar("SB10_TextEntry5_sb32",d);
+
+var e = d.trim();
+player.SetVar("SB10_TextEntry5_sb32",e);
+}
+
+function Script8()
+{
+  var player = GetPlayer();
+
+var a= player.GetVar("SB10_TextEntry5_sb32");
+
+var c= a+"₃";
+
+player.SetVar("SB10_TextEntry5_sb32",c);
+
+var d= c+" ";
+
+player.SetVar("SB10_TextEntry5_sb32",d);
+
+var e = d.trim();
+player.SetVar("SB10_TextEntry5_sb32",e);
+}
+
+function Script9()
+{
+  var els = document.getElementsByTagName('input');
+for (var i=0; i < els.length; i++) {
+ els[i].setAttribute("autocomplete", "off");
+}
+}
+
+function Script10()
+{
+  var player = GetPlayer();
+
+var a= player.GetVar("SB10_TextEntry6_sb33");
+
+var c= a+"₂";
+
+player.SetVar("SB10_TextEntry6_sb33",c);
+
+var d= c+" ";
+
+player.SetVar("SB10_TextEntry6_sb33",d);
+
+var e = d.trim();
+player.SetVar("SB10_TextEntry6_sb33",e);
+}
+
+function Script11()
+{
+  var player = GetPlayer();
+
+var a= player.GetVar("SB10_TextEntry6_sb33");
+
+var c= a+"₄";
+
+player.SetVar("SB10_TextEntry6_sb33",c);
+
+var d= c+" ";
+
+player.SetVar("SB10_TextEntry6_sb33",d);
+
+var e = d.trim();
+player.SetVar("SB10_TextEntry6_sb33",e);
+}
+
+function Script12()
+{
+  var player = GetPlayer();
+
+var a= player.GetVar("SB10_TextEntry6_sb33");
+
+var c= a+"₃";
+
+player.SetVar("SB10_TextEntry6_sb33",c);
+
+var d= c+" ";
+
+player.SetVar("SB10_TextEntry6_sb33",d);
+
+var e = d.trim();
+player.SetVar("SB10_TextEntry6_sb33",e);
+}
+
+function Script13()
+{
+  var els = document.getElementsByTagName('input');
+for (var i=0; i < els.length; i++) {
+ els[i].setAttribute("autocomplete", "off");
+}
+}
+
+function Script14()
+{
+  var player = GetPlayer();
+
+var a= player.GetVar("SB10_TextEntry7_sb34");
+
+var c= a+"₂";
+
+player.SetVar("SB10_TextEntry7_sb34",c);
+
+var d= c+" ";
+
+player.SetVar("SB10_TextEntry7_sb34",d);
+
+var e = d.trim();
+player.SetVar("SB10_TextEntry7_sb34",e);
+}
+
+function Script15()
+{
+  var player = GetPlayer();
+
+var a= player.GetVar("SB10_TextEntry7_sb34");
+
+var c= a+"₄";
+
+player.SetVar("SB10_TextEntry7_sb34",c);
+
+var d= c+" ";
+
+player.SetVar("SB10_TextEntry7_sb34",d);
+
+var e = d.trim();
+player.SetVar("SB10_TextEntry7_sb34",e);
+}
+
+function Script16()
+{
+  var player = GetPlayer();
+
+var a= player.GetVar("SB10_TextEntry7_sb34");
+
+var c= a+"₃";
+
+player.SetVar("SB10_TextEntry7_sb34",c);
+
+var d= c+" ";
+
+player.SetVar("SB10_TextEntry7_sb34",d);
+
+var e = d.trim();
+player.SetVar("SB10_TextEntry7_sb34",e);
+}
+
+function Script17()
+{
+  var els = document.getElementsByTagName('input');
+for (var i=0; i < els.length; i++) {
+ els[i].setAttribute("autocomplete", "off");
+}
+}
+
+function Script18()
+{
+  var player = GetPlayer();
+
+var a= player.GetVar("SB10_TextEntry8_sb35");
+
+var c= a+"₂";
+
+player.SetVar("SB10_TextEntry8_sb35",c);
+
+var d= c+" ";
+
+player.SetVar("SB10_TextEntry8_sb35",d);
+
+var e = d.trim();
+player.SetVar("SB10_TextEntry8_sb35",e);
+}
+
+function Script19()
+{
+  var player = GetPlayer();
+
+var a= player.GetVar("SB10_TextEntry8_sb35");
+
+var c= a+"₄";
+
+player.SetVar("SB10_TextEntry8_sb35",c);
+
+var d= c+" ";
+
+player.SetVar("SB10_TextEntry8_sb35",d);
+
+var e = d.trim();
+player.SetVar("SB10_TextEntry8_sb35",e);
+}
+
+function Script20()
+{
+  var player = GetPlayer();
+
+var a= player.GetVar("SB10_TextEntry8_sb35");
+
+var c= a+"₃";
+
+player.SetVar("SB10_TextEntry8_sb35",c);
+
+var d= c+" ";
+
+player.SetVar("SB10_TextEntry8_sb35",d);
+
+var e = d.trim();
+player.SetVar("SB10_TextEntry8_sb35",e);
+}
+
+function Script21()
+{
+  var els = document.getElementsByTagName('input');
+for (var i=0; i < els.length; i++) {
+ els[i].setAttribute("autocomplete", "off");
+}
+}
+
+function Script22()
+{
+  var player = GetPlayer();
+
+var a= player.GetVar("SB10_TextEntry9_sb36");
+
+var c= a+"₂";
+
+player.SetVar("SB10_TextEntry9_sb36",c);
+
+var d= c+" ";
+
+player.SetVar("SB10_TextEntry9_sb36",d);
+
+var e = d.trim();
+player.SetVar("SB10_TextEntry9_sb36",e);
+}
+
+function Script23()
+{
+  var player = GetPlayer();
+
+var a= player.GetVar("SB10_TextEntry9_sb36");
+
+var c= a+"₄";
+
+player.SetVar("SB10_TextEntry9_sb36",c);
+
+var d= c+" ";
+
+player.SetVar("SB10_TextEntry9_sb36",d);
+
+var e = d.trim();
+player.SetVar("SB10_TextEntry9_sb36",e);
+}
+
+function Script24()
+{
+  var player = GetPlayer();
+
+var a= player.GetVar("SB10_TextEntry9_sb36");
+
+var c= a+"₃";
+
+player.SetVar("SB10_TextEntry9_sb36",c);
+
+var d= c+" ";
+
+player.SetVar("SB10_TextEntry9_sb36",d);
+
+var e = d.trim();
+player.SetVar("SB10_TextEntry9_sb36",e);
+}
+
+function Script25()
+{
+  var els = document.getElementsByTagName('input');
+for (var i=0; i < els.length; i++) {
+ els[i].setAttribute("autocomplete", "off");
+}
+}
+
+function Script26()
+{
+  var player = GetPlayer();
+
+var a= player.GetVar("SB10_TextEntry10_sb37");
+
+var c= a+"₂";
+
+player.SetVar("SB10_TextEntry10_sb37",c);
+
+var d= c+" ";
+
+player.SetVar("SB10_TextEntry10_sb37",d);
+
+var e = d.trim();
+player.SetVar("SB10_TextEntry10_sb37",e);
+}
+
+function Script27()
+{
+  var player = GetPlayer();
+
+var a= player.GetVar("SB10_TextEntry10_sb37");
+
+var c= a+"₄";
+
+player.SetVar("SB10_TextEntry10_sb37",c);
+
+var d= c+" ";
+
+player.SetVar("SB10_TextEntry10_sb37",d);
+
+var e = d.trim();
+player.SetVar("SB10_TextEntry10_sb37",e);
+}
+
+function Script28()
+{
+  var player = GetPlayer();
+
+var a= player.GetVar("SB10_TextEntry10_sb37");
+
+var c= a+"₃";
+
+player.SetVar("SB10_TextEntry10_sb37",c);
+
+var d= c+" ";
+
+player.SetVar("SB10_TextEntry10_sb37",d);
+
+var e = d.trim();
+player.SetVar("SB10_TextEntry10_sb37",e);
+}
+
+function Script29()
+{
+  var els = document.getElementsByTagName('input');
+for (var i=0; i < els.length; i++) {
+ els[i].setAttribute("autocomplete", "off");
+}
+}
+
+function Script30()
+{
+  var player = GetPlayer();
+
+var a= player.GetVar("SB10_TextEntry1");
+
+var c= a+"⁺";
+
+player.SetVar("SB10_TextEntry1",c);
+
+var d= c+" ";
+
+player.SetVar("SB10_TextEntry1",d);
+
+var e = d.trim();
+player.SetVar("SB10_TextEntry1",e);
+}
+
+function Script31()
+{
+  var player = GetPlayer();
+
+var a= player.GetVar("SB10_TextEntry1");
+
+var c= a+"²⁺";
+
+player.SetVar("SB10_TextEntry1",c);
+
+var d= c+" ";
+
+player.SetVar("SB10_TextEntry1",d);
+
+var e = d.trim();
+player.SetVar("SB10_TextEntry1",e);
+}
+
+function Script32()
+{
+  var player = GetPlayer();
+
+var a= player.GetVar("SB10_TextEntry1");
+
+var c= a+"³⁺";
+
+player.SetVar("SB10_TextEntry1",c);
+
+var d= c+" ";
+
+player.SetVar("SB10_TextEntry1",d);
+
+var e = d.trim();
+player.SetVar("SB10_TextEntry1",e);
+
+}
+
+function Script33()
+{
+  var player = GetPlayer();
+
+var a= player.GetVar("SB10_TextEntry1");
+
+var c= a+"⁻";
+
+player.SetVar("SB10_TextEntry1",c);
+
+var d= c+" ";
+
+player.SetVar("SB10_TextEntry1",d);
+
+var e = d.trim();
+player.SetVar("SB10_TextEntry1",e);
+}
+
+function Script34()
+{
+  var player = GetPlayer();
+
+var a= player.GetVar("SB10_TextEntry1");
+
+var c= a+"²⁻";
+
+player.SetVar("SB10_TextEntry1",c);
+
+var d= c+" ";
+
+player.SetVar("SB10_TextEntry1",d);
+
+var e = d.trim();
+player.SetVar("SB10_TextEntry1",e);
+}
+
+function Script35()
+{
+  var player = GetPlayer();
+
+var a= player.GetVar("SB10_TextEntry1");
+
+var c= a+"₂";
+
+player.SetVar("SB10_TextEntry1",c);
+
+var d= c+" ";
+
+player.SetVar("SB10_TextEntry1",d);
+
+var e = d.trim();
+player.SetVar("SB10_TextEntry1",e);
+}
+
+function Script36()
+{
+  var player = GetPlayer();
+
+var a= player.GetVar("SB10_TextEntry1");
+
+var c= a+"₄";
+
+player.SetVar("SB10_TextEntry1",c);
+
+var d= c+" ";
+
+player.SetVar("SB10_TextEntry1",d);
+
+var e = d.trim();
+player.SetVar("SB10_TextEntry1",e);
+}
+
+function Script37()
+{
+  var player = GetPlayer();
+
+var a= player.GetVar("SB10_TextEntry1");
+
+var c= a+"₃";
+
+player.SetVar("SB10_TextEntry1",c);
+
+var d= c+" ";
+
+player.SetVar("SB10_TextEntry1",d);
+
+var e = d.trim();
+player.SetVar("SB10_TextEntry1",e);
+}
+
+function Script38()
+{
+  var els = document.getElementsByTagName('input');
+for (var i=0; i < els.length; i++) {
+ els[i].setAttribute("autocomplete", "off");
+}
+}
+
+function Script39()
+{
+  var player = GetPlayer();
+
+var a= player.GetVar("SB10_TextEntry2");
+
+var c= a+"⁺";
+
+player.SetVar("SB10_TextEntry2",c);
+
+var d= c+" ";
+
+player.SetVar("SB10_TextEntry2",d);
+
+var e = d.trim();
+player.SetVar("SB10_TextEntry2",e);
+}
+
+function Script40()
+{
+  var player = GetPlayer();
+
+var a= player.GetVar("SB10_TextEntry2");
+
+var c= a+"²⁺";
+
+player.SetVar("SB10_TextEntry2",c);
+
+var d= c+" ";
+
+player.SetVar("SB10_TextEntry2",d);
+
+var e = d.trim();
+player.SetVar("SB10_TextEntry2",e);
+}
+
+function Script41()
+{
+  var player = GetPlayer();
+
+var a= player.GetVar("SB10_TextEntry2");
+
+var c= a+"³⁺";
+
+player.SetVar("SB10_TextEntry2",c);
+
+var d= c+" ";
+
+player.SetVar("SB10_TextEntry2",d);
+
+var e = d.trim();
+player.SetVar("SB10_TextEntry2",e);
+}
+
+function Script42()
+{
+  var player = GetPlayer();
+
+var a= player.GetVar("SB10_TextEntry2");
+
+var c= a+"⁻";
+
+player.SetVar("SB10_TextEntry2",c);
+
+var d= c+" ";
+
+player.SetVar("SB10_TextEntry2",d);
+
+var e = d.trim();
+player.SetVar("SB10_TextEntry2",e);
+}
+
+function Script43()
+{
+  var player = GetPlayer();
+
+var a= player.GetVar("SB10_TextEntry2");
+
+var c= a+"²⁻";
+
+player.SetVar("SB10_TextEntry2",c);
+
+var d= c+" ";
+
+player.SetVar("SB10_TextEntry2",d);
+
+var e = d.trim();
+player.SetVar("SB10_TextEntry2",e);
+}
+
+function Script44()
+{
+  var player = GetPlayer();
+
+var a= player.GetVar("SB10_TextEntry2");
+
+var c= a+"₂";
+
+player.SetVar("SB10_TextEntry2",c);
+
+var d= c+" ";
+
+player.SetVar("SB10_TextEntry2",d);
+
+var e = d.trim();
+player.SetVar("SB10_TextEntry2",e);
+}
+
+function Script45()
+{
+  var player = GetPlayer();
+
+var a= player.GetVar("SB10_TextEntry2");
+
+var c= a+"₄";
+
+player.SetVar("SB10_TextEntry2",c);
+
+var d= c+" ";
+
+player.SetVar("SB10_TextEntry2",d);
+
+var e = d.trim();
+player.SetVar("SB10_TextEntry2",e);
+}
+
+function Script46()
+{
+  var player = GetPlayer();
+
+var a= player.GetVar("SB10_TextEntry2");
+
+var c= a+"₃";
+
+player.SetVar("SB10_TextEntry2",c);
+
+var d= c+" ";
+
+player.SetVar("SB10_TextEntry2",d);
+
+var e = d.trim();
+player.SetVar("SB10_TextEntry2",e);
+}
+
+function Script47()
+{
+  var els = document.getElementsByTagName('input');
+for (var i=0; i < els.length; i++) {
+ els[i].setAttribute("autocomplete", "off");
+}
+}
+
+function Script48()
+{
+  var player = GetPlayer();
+
+var a= player.GetVar("SB10_TextEntry3");
+
+var c= a+"⁺";
+
+player.SetVar("SB10_TextEntry3",c);
+
+var d= c+" ";
+
+player.SetVar("SB10_TextEntry3",d);
+
+var e = d.trim();
+player.SetVar("SB10_TextEntry3",e);
+}
+
+function Script49()
+{
+  var player = GetPlayer();
+
+var a= player.GetVar("SB10_TextEntry3");
+
+var c= a+"²⁺";
+
+player.SetVar("SB10_TextEntry3",c);
+
+var d= c+" ";
+
+player.SetVar("SB10_TextEntry3",d);
+
+var e = d.trim();
+player.SetVar("SB10_TextEntry3",e);
+}
+
+function Script50()
+{
+  var player = GetPlayer();
+
+var a= player.GetVar("SB10_TextEntry3");
+
+var c= a+"³⁺";
+
+player.SetVar("SB10_TextEntry3",c);
+
+var d= c+" ";
+
+player.SetVar("SB10_TextEntry3",d);
+
+var e = d.trim();
+player.SetVar("SB10_TextEntry3",e);
+}
+
+function Script51()
+{
+  var player = GetPlayer();
+
+var a= player.GetVar("SB10_TextEntry3");
+
+var c= a+"⁻";
+
+player.SetVar("SB10_TextEntry3",c);
+
+var d= c+" ";
+
+player.SetVar("SB10_TextEntry3",d);
+
+var e = d.trim();
+player.SetVar("SB10_TextEntry3",e);
+}
+
+function Script52()
+{
+  var player = GetPlayer();
+
+var a= player.GetVar("SB10_TextEntry3");
+
+var c= a+"²⁻";
+
+player.SetVar("SB10_TextEntry3",c);
+
+var d= c+" ";
+
+player.SetVar("SB10_TextEntry3",d);
+
+var e = d.trim();
+player.SetVar("SB10_TextEntry3",e);
+}
+
+function Script53()
+{
+  var player = GetPlayer();
+
+var a= player.GetVar("SB10_TextEntry3");
+
+var c= a+"₂";
+
+player.SetVar("SB10_TextEntry3",c);
+
+var d= c+" ";
+
+player.SetVar("SB10_TextEntry3",d);
+
+var e = d.trim();
+player.SetVar("SB10_TextEntry3",e);
+}
+
+function Script54()
+{
+  var player = GetPlayer();
+
+var a= player.GetVar("SB10_TextEntry3");
+
+var c= a+"₄";
+
+player.SetVar("SB10_TextEntry3",c);
+
+var d= c+" ";
+
+player.SetVar("SB10_TextEntry3",d);
+
+var e = d.trim();
+player.SetVar("SB10_TextEntry3",e);
+}
+
+function Script55()
+{
+  var player = GetPlayer();
+
+var a= player.GetVar("SB10_TextEntry3");
+
+var c= a+"₃";
+
+player.SetVar("SB10_TextEntry3",c);
+
+var d= c+" ";
+
+player.SetVar("SB10_TextEntry3",d);
+
+var e = d.trim();
+player.SetVar("SB10_TextEntry3",e);
+}
+
---
+++ 4_quiz/story_content/user.js
@@ -0,0 +1,454 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "6LdQnphvEC8":
+        Script1();
+        break;
+      case "6UTvruQIf4S":
+        Script2();
+        break;
+      case "5j1QKkDs1Qc":
+        Script3();
+        break;
+      case "6BcOwVA0bS3":
+        Script4();
+        break;
+      case "5moxp5RL3vf":
+        Script5();
+        break;
+      case "5nVVxBQOlMw":
+        Script6();
+        break;
+      case "6pzoPD65cq4":
+        Script7();
+        break;
+      case "6TnwpRO5XRu":
+        Script8();
+        break;
+      case "6OiIdcJVPDx":
+        Script9();
+        break;
+      case "5jIvXsGQuqB":
+        Script10();
+        break;
+      case "69rwo70PmB1":
+        Script11();
+        break;
+      case "6fWr7AoBQ1u":
+        Script12();
+        break;
+      case "6efFWcfcoRV":
+        Script13();
+        break;
+      case "5fkVt4jbmMW":
+        Script14();
+        break;
+      case "5e3TY5WoRTu":
+        Script15();
+        break;
+      case "6o5qIz4MG4L":
+        Script16();
+        break;
+      case "6o4iMXtUgJ9":
+        Script17();
+        break;
+      case "5ux4HCu32wi":
+        Script18();
+        break;
+      case "6Ct8fc7igt8":
+        Script19();
+        break;
+      case "6hSO84jN0ML":
+        Script20();
+        break;
+      case "5WSdGxRhsYd":
+        Script21();
+        break;
+      case "6UNRNlvb0AY":
+        Script22();
+        break;
+      case "5XIw8iV53iQ":
+        Script23();
+        break;
+      case "6LZYyoTuHDG":
+        Script24();
+        break;
+      case "6eA3uJp8tWS":
+        Script25();
+        break;
+      case "6TR32k87cMj":
+        Script26();
+        break;
+      case "5s86Pi7PNi6":
+        Script27();
+        break;
+  }
+}
+
+function Script1()
+{
+  var els = document.getElementsByTagName('input');
+for (var i=0; i < els.length; i++) {
+ els[i].setAttribute("autocomplete", "off");
+}
+}
+
+function Script2()
+{
+  var els = document.getElementsByTagName('input');
+for (var i=0; i < els.length; i++) {
+ els[i].setAttribute("autocomplete", "off");
+}
+}
+
+function Script3()
+{
+  var p = GetPlayer();
+var textentry = p.GetVar("Q2_TextEntry1_sb5");
+
+p.SetVar("Q2_TextEntry1_sb5", textentry.replace(/\s/g,''));
+}
+
+function Script4()
+{
+  var player = GetPlayer();
+
+var a= player.GetVar("Q2_TextEntry1_sb5");
+
+var c= a+"₂";
+
+player.SetVar("Q2_TextEntry1_sb5",c);
+
+var d= c+" ";
+
+player.SetVar("Q2_TextEntry1_sb5",d);
+
+var e = d.trim();
+player.SetVar("Q2_TextEntry1_sb5",e);
+}
+
+function Script5()
+{
+  var player = GetPlayer();
+
+var a= player.GetVar("Q2_TextEntry1_sb5");
+
+var c= a+"₄";
+
+player.SetVar("Q2_TextEntry1_sb5",c);
+
+var d= c+" ";
+
+player.SetVar("Q2_TextEntry1_sb5",d);
+
+var e = d.trim();
+player.SetVar("Q2_TextEntry1_sb5",e);
+}
+
+function Script6()
+{
+  var player = GetPlayer();
+
+var a= player.GetVar("Q2_TextEntry1_sb5");
+
+var c= a+"₃";
+
+player.SetVar("Q2_TextEntry1_sb5",c);
+
+var d= c+" ";
+
+player.SetVar("Q2_TextEntry1_sb5",d);
+
+var e = d.trim();
+player.SetVar("Q2_TextEntry1_sb5",e);
+}
+
+function Script7()
+{
+  var els = document.getElementsByTagName('input');
+for (var i=0; i < els.length; i++) {
+ els[i].setAttribute("autocomplete", "off");
+}
+}
+
+function Script8()
+{
+  var p = GetPlayer();
+var textentry = p.GetVar("Q3_TextEntry2_sb6");
+
+p.SetVar("Q3_TextEntry2_sb6", textentry.replace(/\s/g,''));
+}
+
+function Script9()
+{
+  var player = GetPlayer();
+
+var a= player.GetVar("Q3_TextEntry2_sb6");
+
+var c= a+"₂";
+
+player.SetVar("Q3_TextEntry2_sb6",c);
+
+var d= c+" ";
+
+player.SetVar("Q3_TextEntry2_sb6",d);
+
+var e = d.trim();
+player.SetVar("Q3_TextEntry2_sb6",e);
+}
+
+function Script10()
+{
+  var player = GetPlayer();
+
+var a= player.GetVar("Q3_TextEntry2_sb6");
+
+var c= a+"₄";
+
+player.SetVar("Q3_TextEntry2_sb6",c);
+
+var d= c+" ";
+
+player.SetVar("Q3_TextEntry2_sb6",d);
+
+var e = d.trim();
+player.SetVar("Q3_TextEntry2_sb6",e);
+}
+
+function Script11()
+{
+  var player = GetPlayer();
+
+var a= player.GetVar("Q3_TextEntry2_sb6");
+
+var c= a+"₃";
+
+player.SetVar("Q3_TextEntry2_sb6",c);
+
+var d= c+" ";
+
+player.SetVar("Q3_TextEntry2_sb6",d);
+
+var e = d.trim();
+player.SetVar("Q3_TextEntry2_sb6",e);
+}
+
+function Script12()
+{
+  var els = document.getElementsByTagName('input');
+for (var i=0; i < els.length; i++) {
+ els[i].setAttribute("autocomplete", "off");
+}
+}
+
+function Script13()
+{
+  var p = GetPlayer();
+var textentry = p.GetVar("Q4_TextEntry3_sb7");
+
+p.SetVar("Q4_TextEntry3_sb7", textentry.replace(/\s/g,''));
+}
+
+function Script14()
+{
+  var player = GetPlayer();
+
+var a= player.GetVar("Q4_TextEntry3_sb7");
+
+var c= a+"₂";
+
+player.SetVar("Q4_TextEntry3_sb7",c);
+
+var d= c+" ";
+
+player.SetVar("Q4_TextEntry3_sb7",d);
+
+var e = d.trim();
+player.SetVar("Q4_TextEntry3_sb7",e);
+}
+
+function Script15()
+{
+  var player = GetPlayer();
+
+var a= player.GetVar("Q4_TextEntry3_sb7");
+
+var c= a+"₄";
+
+player.SetVar("Q4_TextEntry3_sb7",c);
+
+var d= c+" ";
+
+player.SetVar("Q4_TextEntry3_sb7",d);
+
+var e = d.trim();
+player.SetVar("Q4_TextEntry3_sb7",e);
+}
+
+function Script16()
+{
+  var player = GetPlayer();
+
+var a= player.GetVar("Q4_TextEntry3_sb7");
+
+var c= a+"₃";
+
+player.SetVar("Q4_TextEntry3_sb7",c);
+
+var d= c+" ";
+
+player.SetVar("Q4_TextEntry3_sb7",d);
+
+var e = d.trim();
+player.SetVar("Q4_TextEntry3_sb7",e);
+}
+
+function Script17()
+{
+  var els = document.getElementsByTagName('input');
+for (var i=0; i < els.length; i++) {
+ els[i].setAttribute("autocomplete", "off");
+}
+}
+
+function Script18()
+{
+  var els = document.getElementsByTagName('input');
+for (var i=0; i < els.length; i++) {
+ els[i].setAttribute("autocomplete", "off");
+}
+}
+
+function Script19()
+{
+  var els = document.getElementsByTagName('input');
+for (var i=0; i < els.length; i++) {
+ els[i].setAttribute("autocomplete", "off");
+}
+}
+
+function Script20()
+{
+  var player = GetPlayer();
+
+var a= player.GetVar("TextEntry");
+
+var c= a+"₂";
+
+player.SetVar("TextEntry",c);
+
+var d= c+" ";
+
+player.SetVar("TextEntry",d);
+
+var e = d.trim();
+player.SetVar("TextEntry",e);
+}
+
+function Script21()
+{
+  var player = GetPlayer();
+
+var a= player.GetVar("TextEntry");
+
+var c= a+"₄";
+
+player.SetVar("TextEntry",c);
+
+var d= c+" ";
+
+player.SetVar("TextEntry",d);
+
+var e = d.trim();
+player.SetVar("TextEntry",e);
+}
+
+function Script22()
+{
+  var player = GetPlayer();
+
+var a= player.GetVar("TextEntry");
+
+var c= a+"₃";
+
+player.SetVar("TextEntry",c);
+
+var d= c+" ";
+
+player.SetVar("TextEntry",d);
+
+var e = d.trim();
+player.SetVar("TextEntry",e);
+}
+
+function Script23()
+{
+  var player = GetPlayer();
+
+var a= player.GetVar("SB_TextEntry3");
+
+var c= a+"₂";
+
+player.SetVar("SB_TextEntry3",c);
+
+var d= c+" ";
+
+player.SetVar("SB_TextEntry3",d);
+
+var e = d.trim();
+player.SetVar("SB_TextEntry3",e);
+}
+
+function Script24()
+{
+  var player = GetPlayer();
+
+var a= player.GetVar("SB_TextEntry3");
+
+var c= a+"₄";
+
+player.SetVar("SB_TextEntry3",c);
+
+var d= c+" ";
+
+player.SetVar("SB_TextEntry3",d);
+
+var e = d.trim();
+player.SetVar("SB_TextEntry3",e);
+}
+
+function Script25()
+{
+  var player = GetPlayer();
+
+var a= player.GetVar("SB_TextEntry3");
+
+var c= a+"₃";
+
+player.SetVar("SB_TextEntry3",c);
+
+var d= c+" ";
+
+player.SetVar("SB_TextEntry3",d);
+
+var e = d.trim();
+player.SetVar("SB_TextEntry3",e);
+}
+
+function Script26()
+{
+  var p = GetPlayer();
+var textentry = p.GetVar("SB_TextEntry3");
+
+p.SetVar("SB_TextEntry3", textentry.replace(/\s/g,''));
+}
+
+function Script27()
+{
+  var p = GetPlayer();
+var textentry = p.GetVar("TextEntry");
+
+p.SetVar("TextEntry", textentry.replace(/\s/g,''));
+}
+
